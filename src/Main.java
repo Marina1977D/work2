0f3dc20be022d3f8ea2b3e87fec93392b849129d
Marina1977D <deevamariha@gmail.com>
2022-06-23 16:45:26 +0300

    расчет бонусов

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 1500;
+        int percent = 20;
+        int bonus = ticketPrice / percent;
+        System.out.println("Ваши бонусы за поездку составили-" + bonus + " miles");
+    }
+}
